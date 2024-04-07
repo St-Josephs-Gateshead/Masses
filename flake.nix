fe5b7c5876a1ea6981ed2a3bad64850c2ae261a5
John Morris <2e0byo@gmail.com>
2024-04-07 10:14:49 +0100

    feat: nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Pipelines run by prefect";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        # 'build time' deps
+        buildInputs = with pkgs; [
+          python311
+          copier
+          pre-commit
+          nixpkgs.texlive.combined.scheme-medium
+        ];
+        # allow building c extensions
+        env = {
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit buildInputs;
+            inherit env;
+          };
+        }
+    );
+}
