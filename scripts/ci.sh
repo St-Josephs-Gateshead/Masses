b425fe7e1d65619fd02d0f81740b582cbf0c778e
John Morris <2e0byo@gmail.com>
2025-05-26 18:23:48 +0200

    ci: move to script for easier local reproduction.

---
+++ scripts/ci.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Treat unset variables as an error.
+set -u
+
+# Exit on error in pipeline (e.g. cmd1 | cmd2)
+set -o pipefail
+
+JOBS=
+
+monitor() {
+    while true
+    do
+        free -h
+        w
+        sleep 30
+    done &
+}
+
+init() {
+    git config --global --add safe.directory "$GITHUB_WORKSPACE"
+    bash scripts/post-xxx-sample.txt # gitinfo2 script
+}
+
+configure() {
+    python scripts/configure.py
+
+    echo "Generated makefile:"
+    cat makefile
+}
+
+build () {
+    make -j $JOBS || make -j $JOBS
+    python scripts/package.py
+}
+
+monitor
+init
+configure
+build
+
+
+
